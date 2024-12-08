f75e413784b68012e88039d2c314b3dc34fb1d15
Sanjay-S46 <sanjay2003877@gmail.com>
2024-12-09 00:11:29 +0530

    concurrency for verifier

--- src/com/loanapp/actions/borrower/LoanAction.java
+++ src/com/loanapp/actions/borrower/LoanAction.java
@@ -3,6 +3,7 @@ package com.loanapp.actions.borrower;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 import javax.servlet.http.HttpSession;
@@ -128,6 +129,7 @@ public class LoanAction extends ActionSupport{
                 setLoanId(resultSet.getInt(1));
                 long balanceAmount = resultSet.getLong(2) - resultSet.getLong(5);
                 String status;
+
                 //new condition added, but it may not be neccessary because in LoanDetailAction we filter that in loanStatusFromLender()
                 if (balanceAmount < 0) {
                     System.out.println("Loan cannot be accepted");
@@ -152,7 +154,6 @@ public class LoanAction extends ActionSupport{
 
                 System.out.println("All the details are displayed from the tables");
 
-                // checkCanAcceptLoan();
                 acceptLoan(status,balanceAmount);
             }
         } 
@@ -161,67 +162,110 @@ public class LoanAction extends ActionSupport{
         }
     }
 
-    // method to check if the particular loan can be accepted or not
-    // private boolean checkCanAcceptLoan(){
-    //     String query = "";
-    //     try (
-    //         Connection conn = db.getConnection();
-    //         PreparedStatement preparedStatement = conn.prepareStatement(query);
-    //     ) {
-    //         return true;
-    //     } 
-    //     catch (Exception e) {
-    //         e.printStackTrace();
-    //     }
-    //     return false;
-    // }
-
     // accepting the loan that the borrower selected
-    private void acceptLoan(String status, long balanceAmount){
+    private void acceptLoan(String status, long balanceAmount) throws ClassNotFoundException, SQLException{
 
-        String query = "update loans set status = ?, balance_amount = ?, updated_at = CURRENT_TIMESTAMP where loan_id = ?";
-        try (
-            Connection conn = db.getConnection();
+        Connection conn = db.getConnection();
+
+        try {
+
+            int loanId = getLoanId();
+            long loanGrantedAmount = getLoanGrantedAmount();
+
+            // query for updating the loans table 
+            String query = "update loans set status = ?, balance_amount = ?, updated_at = CURRENT_TIMESTAMP where loan_id = ?";
+
             PreparedStatement preparedStatement = conn.prepareStatement(query);
-        ) {
+
+            conn.setAutoCommit(false); // by setting the auto-commit false we are using the mysql transaction 
+
             preparedStatement.setString(1, status);
             preparedStatement.setLong(2, balanceAmount);
-            preparedStatement.setInt(3, getLoanId());
+            preparedStatement.setInt(3, loanId);
 
-            int result = preparedStatement.executeUpdate();
+            preparedStatement.executeUpdate();
 
-            if (result > 0) {
-                System.out.println("Loans table status has been updated..");
-                updateLenderAmount();
-                updateLoanBalance();
-                updateLoanDistribution();
-                updateDueDate();
-                runScheduler();
+            System.out.println("Loans table status has been updated..");
+
+
+            // updating the lender table's fund amount that is deduct from it
+            String updateLenderAmount = "update lenders set available_funds = ? where lender_id = ?";
+            preparedStatement = conn.prepareStatement(updateLenderAmount);
+
+            long remainingFund = getAvailableFunds() - loanGrantedAmount;
+
+            preparedStatement.setLong(1, remainingFund);
+            preparedStatement.setInt(2, getLenderId());
+
+            preparedStatement.executeUpdate();
+
+            System.out.println("Lenders amount value has been updated..");
+            
+            // to be checked later
+            boolean isLenderHistoryUpdated = updateTransactionHistory(conn,"lender", getLenderUserId());
+            if (!isLenderHistoryUpdated) {
+                throw new Exception();
             }
-        } 
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 
-    // method for activating mysql event scheduler that adds the emi every 1 month into the table
-    private void runScheduler() {
 
-        String eventName = "emi_for_loan_" + getLoanId() + "_with_lender_" + getLenderId();
-        int loanId = getLoanId();
-        int distId = getDistributionId();
-    
-        String endTimeQuery = "select DATE_SUB(loan_due_date, INTERVAL 1 MONTH) AS end_time FROM loans WHERE loan_id = " + loanId;
-    
-        try (
-            Connection conn = db.getConnection();
+            // update the borrower's table's current loan balance amount (i.e) it will increase 
+            String updateLoanBalance = "update borrowers set current_loan_balance = ?, max_loan_amount = ? where borrower_id = ?";
+            preparedStatement = conn.prepareStatement(updateLoanBalance);
+
+            long amount = getCurrentBalance() + loanGrantedAmount;
+            long maxAmount = getMaxLoanAmount() - loanGrantedAmount;
+
+            preparedStatement.setLong(1, amount);
+            preparedStatement.setLong(2, maxAmount);
+            preparedStatement.setInt(3, getBorrowerId());
+
+            preparedStatement.executeUpdate();
+            System.out.println("Borrowers current loan balance is updated..");
+
+
+            // to be checked later
+            boolean isBorrowerHistoryUpdated = updateTransactionHistory(conn,"borrower", getBorrowerUserId());
+            if (!isBorrowerHistoryUpdated) {
+                throw new Exception();
+            }
+
+            // query for updating the loan distribution table
+            String updateLoanDistribution = "update loan_distribution set is_loan_accepted=1, remaining_principle_amount=?, emi=?, "
+                                            +" remaining_months=? where distribution_id = ?";
+
+            preparedStatement = conn.prepareStatement(updateLoanDistribution);
+            
+            preparedStatement.setLong(1, loanGrantedAmount);
+            preparedStatement.setInt(2, getEmi());
+            preparedStatement.setInt(3, getMonth());
+            preparedStatement.setInt(4, getDistributionId());
+
+            preparedStatement.executeUpdate();
+            System.out.println("Loan distribution table updated..");
+
+            // query for updating the loan due date
+            String updateDueDate = "update loans set loan_due_date = DATE_ADD(CURRENT_TIMESTAMP, INTERVAL loan_tenure_months MONTH) "
+                                    +" where loan_id=? and loan_due_date IS NULL";
+
+            preparedStatement = conn.prepareStatement(updateDueDate);
+            preparedStatement.setInt(1, loanId);
+
+            preparedStatement.executeUpdate();
+            System.out.println("Due date for the loan successfully fixed..");
+
+            // query for activating mysql event scheduler that adds the emi every 1 month into the table
+
+            String eventName = "emi_for_loan_" + loanId + "_with_lender_" + getLenderId();
+            int distId = getDistributionId();
+            String endTimeQuery = "select DATE_SUB(loan_due_date, INTERVAL 1 MONTH) AS end_time FROM loans WHERE loan_id = " + loanId;
+
             Statement statement = conn.createStatement();
             ResultSet rs = statement.executeQuery(endTimeQuery);
-        ) {
+
             if (rs.next()) {
                 String endTime = rs.getString("end_time");
     
-                String query = "CREATE EVENT " + eventName + " "
+                String createSchedule = "CREATE EVENT " + eventName + " "
                         + "ON SCHEDULE "
                         + "EVERY 1 MONTH "
                         + "STARTS CURRENT_TIMESTAMP "
@@ -235,100 +279,28 @@ public class LoanAction extends ActionSupport{
                         + "    WHERE due_date IS NULL; "
                         + "END;";
     
-                statement.executeUpdate(query);
+                statement.executeUpdate(createSchedule);
                 System.out.println("Scheduler has been set successfully.");
+
+                conn.commit();
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-    
-
-    // updating the lender table's fund amount that is deduct from it
-    private void updateLenderAmount(){
-        String query = "update lenders set available_funds = ? where lender_id = ?";
-        try (
-            Connection conn = db.getConnection();
-            PreparedStatement preparedStatement = conn.prepareStatement(query);
-        ) {
-            long remainingFund = getAvailableFunds() - getLoanGrantedAmount();
-
-            preparedStatement.setLong(1, remainingFund);
-            preparedStatement.setInt(2, getLenderId());
-            int result = preparedStatement.executeUpdate();
-
-            if (result > 0) {
-                System.out.println("Lenders amount value has been updated..");
-                updateTransactionHistory("lender", getLenderUserId());
-                
+            else{
+                conn.rollback();
             }
         } 
         catch (Exception e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    // update the borrower's table's current loan balance amount (i.e) it will increase 
-    private void updateLoanBalance(){
-        String query = "update borrowers set current_loan_balance = ?, max_loan_amount = ? where borrower_id = ?";
-        try (
-            Connection conn = db.getConnection();
-            PreparedStatement preparedStatement = conn.prepareStatement(query);
-        ) {
-            
-            long amount = getCurrentBalance() + getLoanGrantedAmount();
-            long maxAmount = getMaxLoanAmount() - getLoanGrantedAmount();
-
-            preparedStatement.setLong(1, amount);
-            preparedStatement.setLong(2, maxAmount);
-            preparedStatement.setInt(3, getBorrowerId());
-            int result = preparedStatement.executeUpdate();
-
-            if (result > 0) {
-                System.out.println("Borrowers current loan balance is updated..");
-                updateTransactionHistory("borrower", getBorrowerUserId());
-                //insertEMI();
-            }
-
-        } 
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void updateLoanDistribution(){
-        String query = "update loan_distribution set is_loan_accepted=1, remaining_principle_amount=?, emi=?, remaining_months=? "
-                    +" where distribution_id = ?";
-        try (
-            Connection conn = db.getConnection();
-            PreparedStatement preparedStatement = conn.prepareStatement(query);
-        ) {
-
-            preparedStatement.setLong(1, getLoanGrantedAmount());
-            preparedStatement.setInt(2, getEmi());
-            preparedStatement.setInt(3, getMonth());
-            preparedStatement.setInt(4, getDistributionId());
-
-            int result = preparedStatement.executeUpdate();
-
-            if (result > 0) {
-                System.out.println("Loan distribution table updated..");
-            }
-            
-        } 
-        catch (Exception e) {
+            conn.rollback();
+            System.out.println("Some error occured so rolled back..");
             e.printStackTrace();
         }
     }
 
     // update transaction history for the lender
-    private void updateTransactionHistory(String role,int id){
+    private boolean updateTransactionHistory(Connection conn, String role, int id){
 
         String query = "insert into transaction_history (user_id, lender_id, borrower_id, amount, transaction_type) values (?,?,?,?,?)";
 
         try (
-            Connection conn = db.getConnection();
             PreparedStatement preparedStatement = conn.prepareStatement(query);
         ) {
             preparedStatement.setInt(1, id);
@@ -347,32 +319,13 @@ public class LoanAction extends ActionSupport{
 
             if (result > 0) {
                 System.out.println("Transaction table is updated.. ");
+                return true;
             }
         } 
         catch (Exception e) {
             e.printStackTrace();
         }
-    }
-
-    // method for updating the loan due date 
-    private void updateDueDate(){
-        String query = "update loans set loan_due_date = DATE_ADD(CURRENT_TIMESTAMP, INTERVAL loan_tenure_months MONTH) where loan_id=? and "
-                        +" loan_due_date IS NULL";
-        try (
-            Connection conn = db.getConnection();
-            PreparedStatement preparedStatement = conn.prepareStatement(query);
-        ) {
-            preparedStatement.setInt(1, getLoanId());
-
-            int result = preparedStatement.executeUpdate();
-
-            if (result > 0) {
-                System.out.println("Due date for the loan successfully fixed..");
-            }
-        } 
-        catch (Exception e) {
-            e.printStackTrace();
-        }
+        return false;
     }
 
     // method for rejecting the loan
--- src/com/loanapp/actions/verifier/VerifierAction.java
+++ src/com/loanapp/actions/verifier/VerifierAction.java
@@ -3,7 +3,12 @@ package com.loanapp.actions.verifier;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+
+import javax.servlet.http.HttpSession;
+
+import org.apache.struts2.ServletActionContext;
 
 import com.loanapp.DatabaseConnection;
 import com.loanapp.models.User;
@@ -24,6 +29,7 @@ public class VerifierAction extends ActionSupport implements ModelDriven<User>{
     private String username;
     private int userId;
     private String userType;
+    private int verifierId;
 
     private ArrayList<User> borrowers = new ArrayList<>();
     private ArrayList<User> lenders = new ArrayList<>();
@@ -62,6 +68,9 @@ public class VerifierAction extends ActionSupport implements ModelDriven<User>{
     public void setUserId(int userId){
         this.userId = userId;
     }
+    public void setVerifierId(int verifierId){
+        this.verifierId = verifierId;
+    }
 
     //getters
     public ArrayList<User> getBorrowers(){
@@ -89,14 +98,19 @@ public class VerifierAction extends ActionSupport implements ModelDriven<User>{
         getUserId();
         return userId;
     }
+    public int getVerifierId(){
+        return verifierId;
+    }
 
     //query to getting all the borrowes and lenders (users)
     private void getAllUsers(){
-        String query = "select username, emailId, userType, status from users inner join verification on users.user_id=verification.user_id where userType in ('borrower', 'lender') and isBlocked=0 and isVerified=1";
+        String query = "select username, emailId, userType, status from users inner join verification on users.user_id = "
+                    + " verification.user_id where userType in ('borrower','lender') and isBlocked=0 and isVerified=1 and verified_by=?";
         try (
             Connection conn = db.getConnection();
             PreparedStatement preparedStatement = conn.prepareStatement(query);
         ) {
+            preparedStatement.setInt(1, getVerifierId());
 
             ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
@@ -133,7 +147,8 @@ public class VerifierAction extends ActionSupport implements ModelDriven<User>{
 
     //query for getting the new verifiers for verification process
     private void getAllNewUsers(){
-        String query = "select username, emailId, userType, status from users inner join verification on users.user_id=verification.user_id where userType in ('borrower', 'lender') and status in ('In progress')";
+        String query = "select username, emailId, userType, status from users inner join verification on users.user_id = "
+                    + " verification.user_id where userType in ('borrower', 'lender') and status in ('In progress')";
         try (
             Connection conn = db.getConnection();
             PreparedStatement preparedStatement = conn.prepareStatement(query);
@@ -166,12 +181,14 @@ public class VerifierAction extends ActionSupport implements ModelDriven<User>{
 
     // method for getting all the blocked users
     public void getAllBlockedUsers(){
-        String query = "select username,emailId,userType from users where isBlocked=1";
+        String query = "select username,emailId,userType from users inner join verification on users.user_id = verification.user_id "
+                    + " where isBlocked=1 and verified_by = ?";
         try (
             Connection conn = db.getConnection();
             PreparedStatement preparedStatement = conn.prepareStatement(query);
         ) {
-
+            preparedStatement.setInt(1, getVerifierId());
+            
             ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
                 user = new User();
@@ -216,132 +233,180 @@ public class VerifierAction extends ActionSupport implements ModelDriven<User>{
     }
 
     //method for accepting the user
-    public String acceptUser(){ 
-        String query = "update users set isVerified=1, randomSalt=?, password=?  where username=?";
-        try (
-            Connection conn = db.getConnection();
-            PreparedStatement preparedStatement = conn.prepareStatement(query);
-        ) {
-            String pwd = passwordGenertor.generatePassword();
+    public String acceptUser() throws ClassNotFoundException, SQLException{ 
+        
+        String selectQuery = "select emailId from users where username = ? for update " ; 
+        String updateQuery = "update users set isVerified=1, randomSalt=?, password=?  where username=?";
+        String insertQuery = "INSERT INTO " + getUserType() + " (user_id) SELECT user_id FROM users WHERE username = ?";
 
+        String username = getUsername();
+         
+        Connection conn = db.getConnection();
+        
+        try ( 
+            PreparedStatement selectPrepStmt = conn.prepareStatement(selectQuery);
+            PreparedStatement updatePrepStmt = conn.prepareStatement(updateQuery);
+            PreparedStatement insertPrepStmt = conn.prepareStatement(insertQuery);
+        ) {
+            conn.setAutoCommit(false);
+
+            selectPrepStmt.setString(1, username);
+
+            ResultSet resultSet = selectPrepStmt.executeQuery();
+            String email;
+
+            if (resultSet.next()) {
+                email = resultSet.getString(1);
+            }
+            else{
+                conn.rollback();
+                return "error"; 
+            }
+
+            String pwd = passwordGenertor.generatePassword();
             String[] hashedPassword = passwordHasher.generateHashedPassword(pwd);
 
-            preparedStatement.setString(1, hashedPassword[0]);
-            preparedStatement.setString(2, hashedPassword[1]);
-            preparedStatement.setString(3, getUsername());
+            updatePrepStmt.setString(1, hashedPassword[0]);
+            updatePrepStmt.setString(2, hashedPassword[1]);
+            updatePrepStmt.setString(3, username);
 
-            int value = preparedStatement.executeUpdate();
+            int value = updatePrepStmt.executeUpdate();
 
-            if (value > 0) {
+            if (value > 0 && email != null) {
                 // sending the password to the respective user email id
-                sendMail(pwd,getUsername());
+                sendMail(pwd,username,email);
+                boolean isUpdated = updateVerificationTable(conn, "Verified");
 
-                updateVerificationTable("Verified");
+                if (!isUpdated) {
+                    throw new Exception();
+                }
+
+                // after accepting the particular user (lender or borrower) adding to the respective table
+                insertPrepStmt.setString(1, username);
+                insertPrepStmt.executeUpdate();
+
+                System.out.println("Successfully inserted into the " + getUserType() +" table ..");
+                
+                conn.commit();
                 return "success";
             }
             
-        } catch (Exception e) {
+        } 
+        catch (Exception e) {
+            conn.rollback();
             e.printStackTrace();
+        }
+        finally{
+            conn.setAutoCommit(true);
+            conn.close();
         }
         return "error";
     }
 
 
     // sending email to the user with the password  
-    private void sendMail(String password, String uname){
-        String query = "select emailId from users where username=?";
-        try (
-            Connection conn = db.getConnection();
-            PreparedStatement preparedStatement = conn.prepareStatement(query);
-        ) {
-
-            preparedStatement.setString(1, uname);
-            ResultSet resultSet = preparedStatement.executeQuery();
-        
-            if (resultSet.next()) {
-                String email = resultSet.getString("emailId");
-                String subject = "You have been verified";
-                String message =    "<body>" +
-                                        "<p>Greetings, " + uname + "</p>" +
-                                        "<p>You have been verified and can now use the application.</p>" +
-                                        "<p><strong>Your password is: " + password + "</strong></p>" +
-                                        "<p><a href='http://localhost:8080/loanApplication/login.jsp'>Click here to log in</a></p>" +
-                                    "</body>" ;
-
-                mail.sendMailThread(email, subject, message);
-            }
-            else{
-                System.out.println("No user available.");
-            }
-            
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    private void sendMail(String password, String uname, String email) throws InterruptedException {
+        String subject = "You have been verified";
+        String message =    "<body>" +
+                                "<p>Greetings, " + uname + "</p>" +
+                                "<p>You have been verified and can now use the application.</p>" +
+                                "<p><strong>Your password is: " + password + "</strong></p>" +
+                                "<p><a href='http://localhost:8080/loanApplication/login.jsp'>Click here to log in</a></p>" +
+                            "</body>" ;
+        Thread.sleep(300);
+        mail.sendMailThread(email, subject, message);
     }
 
 
     //method for rejecting the user
-    public String rejectUser(){
-        String query = "update users set isVerified=0 where username=?";
+    public String rejectUser() throws ClassNotFoundException, SQLException{
+
+        String selectQuery = "select user_id from users where username = ? for update";
+        String updateQuery = "update users set isVerified=0 where username=?";
+        String username = getUsername();
+
+        Connection conn = db.getConnection();
+
         try (
-            Connection conn = db.getConnection();
-            PreparedStatement preparedStatement = conn.prepareStatement(query);
+            PreparedStatement selectPrepStmt = conn.prepareStatement(selectQuery);
+            PreparedStatement updatePrepStmt = conn.prepareStatement(updateQuery);
         ) {
-            preparedStatement.setString(1, getUsername());
-            int value = preparedStatement.executeUpdate();
+            conn.setAutoCommit(false);
+
+            selectPrepStmt.setString(1, username);
+
+            ResultSet resultSet = selectPrepStmt.executeQuery();
+            if (!resultSet.next()) {
+                conn.rollback();
+                return "error";
+            }
+
+            updatePrepStmt.setString(1, username);
+            int value = updatePrepStmt.executeUpdate();
+
             if (value > 0) {
-                updateVerificationTable("Rejected");
+                boolean isUpdated = updateVerificationTable(conn, "Rejected");
+
+                if (!isUpdated) {
+                    throw new Exception();
+                }
+    
+                conn.commit();
                 return "success";
             }
 
         } catch (Exception e) {
+            conn.rollback();
             e.printStackTrace();
+        }
+        finally{
+            conn.setAutoCommit(true);
+            conn.close();
         }
         return "error";
     }
 
 
     // updating the entry into verification table
-    private void updateVerificationTable(String status){
+    private boolean updateVerificationTable(Connection conn, String status){
 
-        String query = "update verification set verified_by=1 , status=? , verification_date=NOW() where user_id=?";
+        String query = "update verification set verified_by=? , status=? , verification_date=NOW() where user_id=?";
 
         try (
-            Connection conn = db.getConnection();
             PreparedStatement preparedStatement = conn.prepareStatement(query);
         ) {
-            preparedStatement.setString(1, status);
-            preparedStatement.setInt(2, getId());
-            preparedStatement.executeUpdate();
-            insertIntoTable();
+            preparedStatement.setInt(1, getVerifierId());
+            preparedStatement.setString(2, status);
+            preparedStatement.setInt(3, getId());
+            int result = preparedStatement.executeUpdate();
+
+            if (result > 0) {
+                return true;
+            }
         } 
         catch (Exception e) {
             e.printStackTrace();
         }
+        return false;
     }
 
-    // after accepting the particular user (lender or borrower) adding to the respective table
-    private void insertIntoTable(){
-
-        String query = "INSERT INTO " + getUserType() + " (user_id) SELECT user_id FROM users WHERE username = ?";
-        try (
-            Connection conn = db.getConnection();
-            PreparedStatement preparedStatement = conn.prepareStatement(query);
-        ) {
-            preparedStatement.setString(1, getUsername());
-            preparedStatement.executeUpdate();
-        } 
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 
     @Override
     public String execute(){
-        getAllUsers();
-        getAllNewUsers();
-        getAllBlockedUsers();
-        return "success";
+        HttpSession session = ServletActionContext.getRequest().getSession(false);
+        if (session != null) {
+            User sessionUser = (User) session.getAttribute("userSession");
+            if (sessionUser != null) {
+
+                setVerifierId(sessionUser.getUserId());
+
+                getAllUsers();
+                getAllNewUsers();
+                getAllBlockedUsers();
+                return "success";
+            }
+        }
+        return "error";
     }
     
 }
